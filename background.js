b96aab2328e88a112f3a58ef87140610f7e19012
angelgiron <angel.c.giron1@gmail.com>
2022-05-18 18:14:24 -0700

    finished chrome extension

---
+++ background.js
@@ -0,0 +1,7 @@
+// let color = '#3aa757';
+let color = '#';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
